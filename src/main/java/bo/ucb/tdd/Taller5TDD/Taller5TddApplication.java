0365cea00c2d6ff85dcf4a25b1958216ad0e7eef
Stephanie Saavedra <stephanie.saavedra.ayarde@gmail.com>
2022-10-21 10:50:48 -0400

    first commit

---
+++ src/main/java/bo/ucb/tdd/Taller5TDD/Taller5TddApplication.java
@@ -0,0 +1,13 @@
+package bo.ucb.tdd.Taller5TDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller5TddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Taller5TddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bo/ucb/tdd/Taller5TDD/Taller5TddApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.ucb.tdd.Taller5TDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller5TddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
